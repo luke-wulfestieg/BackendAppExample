552b2c3962ed50af0803a3d6c6ffce19765a3799
Luke Wulfestieg <l.wulfestieg@sternico.com>
2022-03-17 10:53:01 +0100

    Initial Commit Backend

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+       
+       
+        // private int myVar;   Propfull
+        // public int MyProperty
+        // {
+        //     get { return myVar; }
+        //     set { myVar = value; }
+        // }
+        
+    }
+}
