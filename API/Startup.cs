c4398a06dd640ebc035fbfbcc15556ef67392b31
Luke Wulfestieg <l.wulfestieg@sternico.com>
2022-03-17 12:34:01 +0100

    Initial Commit Frontend

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,14 @@ namespace API
             });   
 
             services.AddControllers();
+            services.AddCors(options =>
+            {
+            options.AddDefaultPolicy(builder =>
+            {
+            builder.WithOrigins(new[] { "http://localhost:4200" });
+            });
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -51,6 +59,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("*"));
 
             app.UseAuthorization();
 
